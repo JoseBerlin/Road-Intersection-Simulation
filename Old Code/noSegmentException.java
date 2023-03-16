13b357d4f6d3cfedcbdd264c8a081fd8ee4dce40
hoxyshmoo <52104528+hoxyshmoo@users.noreply.github.com>
2023-03-16 23:53:54 +0000

    update

--- Old Code/noSegmentException.java
+++
@@ -1,9 +0,0 @@
-//This exception class checks whether the segment has North, West, South, or East directions.
-// If any other direction is specified in the Vehicle Object, the exception is thrown, stating that "Segment does not exist." 
-public class noSegmentException extends Exception {
-
-    public noSegmentException(String exMessage) {
-        super(exMessage);
-
-    }
-}
